022150a7d24955bf38f610a3bd3504983f773e9d
Hauserrodr <hauserd1@hotmail.com>
2023-08-20 14:52:24 -0300

    GoogleDriveManager: initial commit

---
+++ src/gdm.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+from loguru import logger
+
+# Find this script directory
+script_dir = Path( __file__ ).parent.absolute()
+base_dir = Path( __file__ ).parent.parent.absolute()
+
+
+class GoogleDriveManager:
+    def __init__(self):
+        logger.info('Initializing GoogleDriveManager...')
+        self.auth = self._generate_auth()
+        self.gd = GoogleDrive(self.auth)
+        logger.success('Successfully authenticated and initialized GoogleDriveManager.')
+
+    def _generate_auth(self):
+        auth = GoogleAuth()
+        return auth
+
+    def list_files(self):
+        file_list = self.gd.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+        return file_list
+
+    def upload_file(self, file_path, filename = None, gdrive_path = None):
+        if gdrive_path is not None:
+            folderName = gdrive_path
+            folders = self.gd.ListFile(
+                {'q': "title='" + folderName + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+            for folder in folders:
+                if folder['title'] == folderName:
+                    folder_id = folder['id']
+            if filename is None:
+                file = self.gd.CreateFile()
+            else:
+                file = self.gd.CreateFile({'title': filename})
+        else:
+            if filename is None:
+                file = self.gd.CreateFile()
+            else:
+                file = self.gd.CreateFile({'title': filename})
+        file.SetContentFile(file_path)
+        file.Upload()
+        file.InsertPermission({
+            'type':  'anyone'
+            ,'value': 'anyone'
+            ,'role':  'reader'
+        })
+        return file['alternateLink']
