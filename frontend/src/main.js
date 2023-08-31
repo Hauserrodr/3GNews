9cdd9379228dc4eaae48bebe2635c1d12e59ba0a
Hauserrodr <hauserd1@hotmail.com>
2023-08-31 04:21:15 -0300

    import lazyload for images

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,9 @@
+import VueLazyload from 'vue-lazyload'
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +11,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(VueLazyload, {
+    preLoad: 1.3,  // Preload height ratio, default is 1.3
+    error: 'path/to/error/image.png',  // Path to error image
+    loading: 'path/to/loading/gif_or_image.gif',  // Path to a loading image/gif
+    attempt: 1,  // Number of attempts, default is 3
+    transition: "fade"
+  })
 app.mount('#app')
