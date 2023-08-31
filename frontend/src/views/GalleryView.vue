aceecc2e69ccf4ed0abfa434e285f4acd437519b
Hauserrodr <hauserd1@hotmail.com>
2023-08-31 04:21:15 -0300

    placeholder for gallery page

---
+++ frontend/src/views/GalleryView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="about">
+      <G3Header :doneLoading="true" />
+      <h1>This is a gallery page</h1>
+    </div>
+</template>
+  
+<script>
+import G3Header from '@/components/G3Header.vue';
+export default {
+components: { G3Header }
+}
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+.about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+}
+}
+</style>
